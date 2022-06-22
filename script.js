8d749ec4de78eeed0d21886950759c26a09dd17d
Ethan DeMougin <ethandemougin@gmail.com>
2022-06-22 15:03:51 -0400

    loop for character selections

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 // Assignment code here
 function generatePassword() {
   var specialCharacters = ['@', '%',  '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',' ,')' ,'(' ,'}' ,'{' ,']' ,'[' ,'~' ,'-' ,'_' ,'.'];
-  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var numericalCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   var upperCaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var lowerCaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
   var selectedArray = [];
   var passwordLength = getPasswordLength();
-  
+
+  var charTypeSelected = false;
+    while (charTypeSelected == false) {
+      var specialCharacters = getChoice("specialCharacters");
+      var numericalCharacters = getChoice("numericalCharacters");
+      var upperCaseCharacters = getChoice("upperCaseCharacters");
+      var lowerCaseCharacters = getChoice("lowerCaseCharacters");
+
+      if ( (specialCharacters) || (numericalCharacters) || (upperCaseCharacters) || (lowerCaseCharacters) ) {
+        charTypeSelected = true;
+      
+      } else {
+        window.alert("You have to select at least one of the character types!")
+      }
+    }
+
+    
 }
 
 // Get references to the #generate element
